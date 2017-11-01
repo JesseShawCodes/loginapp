f6dcd6ecc9a536fb5135cbd7364e670de4d47e23
Jesse Shaw <jdshaw1987@gmail.com>
2017-11-01 13:14:24 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expresshbs = require('express-handlebars');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/loginapp');
+const db = mongoose.connection;
+
+const routes = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', expresshbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars'); 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+//express session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+//passport initialization
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+//express validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+        , root = namespace.shift()
+        , formParam = root;
+
+    while (namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+        param: formParam,
+        msg: msg,
+        value: value
+    };
+    }
+}));
+
+//express flash
+app.use(flash());
+
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
+
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function() {
+    console.log('Server started at '+app.get('port'));
+})
---
+++ routes/users.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+//register route
+router.get('/register', function(req, res) {
+    res.render('register');
+});
+
+//login route
+
+router.get('/login', function(req, res) {
+    res.render('login');
+});
+
+module.exports = router;
